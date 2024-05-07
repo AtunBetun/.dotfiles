a725cd250b7cc7df82d60dacad775eaf067e1967
Alberto De Saint Malo <alberto.desaintmalo@blackstone.com>
2024-05-07 11:54:14 -0400

    adding statusline

--- nvim/.config/nvim/lua/custom/plugins/format.lua
+++ nvim/.config/nvim/lua/custom/plugins/format.lua
@@ -28,6 +28,7 @@ return {
 			-- Conform can also run multiple formatters sequentially
 			python = { "isort", "black" },
 			json = { "jq" },
+			bash = { "shfmt" },
 			--
 			-- You can use a sub-list to tell conform to run *until* a formatter
 			-- is found.
